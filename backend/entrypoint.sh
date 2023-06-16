50f9d14a5934cac6479d165a1f25dfff81bb1a5b
Vincent Mbugua <vnjeru85@gmail.com>
2023-06-16 14:28:51 +0300

    modify docker cofig

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
-APP_PORT=${PORT:-8111}
+#!/bin/sh
+
+function deploy(){
+
+	python manage.py migrate
+
+	python manage.py createsuperuser --username ${SUPER_USERNAME} --email ${SUPER_EMAIL} --password ${SUPER_PASSWORD}
+
+    python manage.py runserver 0.0.0.0:8111
+}
 cd /app/
-/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm bizcore.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+
+source /opt/venv/bin/activate
+
+deploy
