1822871a550900b6b92cccfe3faa4828b11fbe4e
Vincent Mbugua <vnjeru85@gmail.com>
2023-06-16 15:09:35 +0300

    modify entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,9 +4,9 @@ function deploy(){
 
 	python manage.py migrate
 
-	python manage.py createsuperuser --username ${SUPER_USERNAME} --email ${SUPER_EMAIL} --password ${SUPER_PASSWORD}
+	# python manage.py createsuperuser --username ${SUPER_USERNAME} --email ${SUPER_EMAIL} --password ${SUPER_PASSWORD}
 
-    python manage.py runserver 0.0.0.0:8111
+    python manage.py runserver 0.0.0.0:${APP_PORT}
 }
 cd /app/
 
