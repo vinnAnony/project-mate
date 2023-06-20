0a7c0b59e5176a8981bf131db75ed2e9b64aac9a
Vincent Mbugua <vnjeru85@gmail.com>
2023-06-20 11:43:53 +0300

    configure acccounts in admin site

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('username', 'password1', 'password2', 'is_staff', 'is_active')}),
+    )
+    
+    
+admin.site.register(User, CustomUserAdmin)
