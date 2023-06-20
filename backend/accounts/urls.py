471e12f8da371e8f9b99ec94032fc5a18ac7cf3b
Vincent Mbugua <vnjeru85@gmail.com>
2023-06-20 14:10:03 +0300

    accounts -todo urls

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -9,6 +9,13 @@ router.register(r'users', views.UserViewSet,basename='user')
 
 urlpatterns = [
     # path('forgot-password/', views.ForgotPasswordView.as_view()),
+    # logout/ [name='logout']
+    # forgot-password/ [name='forgot_password']
+    # password-reset/ [name='password_reset']
+    # reset/<uidb64>/<token>/ [name='password_reset_confirm']
+    # forgot-password/done/ [name='forgot_password_done']=>front-end
+    # password-reset/done/ [name='password_reset_done']=>front-end
+    # reset/done/ [name='password_reset_complete']=>front-end
 ]
 
 urlpatterns += router.urls
