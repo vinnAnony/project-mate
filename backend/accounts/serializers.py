df9c3c265a991c004c6f85724b651e64c5343901
jexi-The Linux <vnjeru85@gmail.com>
2023-06-22 22:43:20 +0300

    finish subscriptions module - admin

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -4,27 +4,32 @@ from django.conf import settings
 
 from .models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
         fields = [
-            'id', 
-            'first_name', 
-            'last_name', 
-            'username', 
-            'email', 
-            'password',
+            "id",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password",
         ]
+
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     """
     Serializer for registration of user
     It ensures that a first user does not exist without an account.
     """
+
     password = serializers.CharField(style={"input_type": "password"}, write_only=True)
-    confirm_password = serializers.CharField(style={"input_type": "password"}, write_only=True)
+    confirm_password = serializers.CharField(
+        style={"input_type": "password"}, write_only=True
+    )
 
     def validate_password(self, value):
         """Validating password length"""
@@ -45,29 +50,38 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         confirm_password = data["confirm_password"]
 
         if password != confirm_password:
-            raise serializers.ValidationError({"confirm_password":"Passwords do not match."})
+            raise serializers.ValidationError(
+                {"confirm_password": "Passwords do not match."}
+            )
 
         return data
-    
+
     def create(self, validated_data):
         user = validated_data
-        password = user['password']
+        password = user["password"]
 
         with transaction.atomic():
             user = User.objects.create_user(
-                username=user['username'], 
-                first_name=user['first_name'], 
-                last_name=user['last_name'], 
-                email=user['email'],
-                password=password
-            )            
+                username=user["username"],
+                first_name=user["first_name"],
+                last_name=user["last_name"],
+                email=user["email"],
+                password=password,
+            )
 
             return user
-        
+
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password', 'confirm_password']
+        fields = [
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password",
+            "confirm_password",
+        ]
         extra_kwargs = {
-            'first_name': {'required': True, 'allow_blank': False},
-            'last_name': {'required': True,'allow_blank': False}
-            }
+            "first_name": {"required": True, "allow_blank": False},
+            "last_name": {"required": True, "allow_blank": False},
+        }
--- backend/subscription/models.py
+++ backend/subscription/models.py
@@ -2,45 +2,56 @@ import uuid
 from django.db import models
 from django.urls import reverse
 
+
 # Create your models here.
 class SubscriptionPackage(models.Model):
     class SubscriptionDuration(models.TextChoices):
-        Day = 'Day'
-        Week = 'Week'
-        Month = 'Month'
-        Year = 'Year'
-        OneTime = 'One-Time'
-        
-    id = models.UUIDField(default=uuid.uuid4,primary_key=True,unique=True)
-    name = models.CharField(max_length=255,null=False,blank=False)
-    description = models.TextField(null=False,blank=False)
-    price = models.DecimalField(max_digits=5, decimal_places=2,null=False,blank=False)
-    duration = models.CharField(max_length=100, choices=SubscriptionDuration.choices,null=False,blank=False)
+        Day = "Day"
+        Week = "Week"
+        Month = "Month"
+        Year = "Year"
+        OneTime = "One-Time"
+
+    id = models.UUIDField(
+        default=uuid.uuid4, primary_key=True, unique=True, editable=False
+    )
+    name = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    price = models.DecimalField(
+        max_digits=11, decimal_places=2, null=False, blank=False
+    )
+    duration = models.CharField(
+        max_length=100, choices=SubscriptionDuration.choices, null=False, blank=False
+    )
     project_id = models.ForeignKey("project.Project", on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = ("Subscription Package")
-        verbose_name_plural = ("Subscription Packages")
+        verbose_name = "Subscription Package"
+        verbose_name_plural = "Subscription Packages"
 
     def __str__(self):
-        return self.name
+        return f"{self.project_id} - {self.name}"
 
     def get_absolute_url(self):
         return reverse("subscription-package-detail", kwargs={"pk": self.pk})
 
 
 class Subscription(models.Model):
-    id = models.UUIDField(default=uuid.uuid4,primary_key=True,unique=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, primary_key=True, unique=True, editable=False
+    )
     customer_id = models.ForeignKey("customer.Customer", on_delete=models.CASCADE)
     project_id = models.ForeignKey("project.Project", on_delete=models.CASCADE)
-    subscription_package_id = models.ForeignKey(SubscriptionPackage , on_delete=models.CASCADE)
+    subscription_package_id = models.ForeignKey(
+        SubscriptionPackage, on_delete=models.CASCADE
+    )
 
     class Meta:
-        verbose_name = ("Subscription")
-        verbose_name_plural = ("Subscriptions")
+        verbose_name = "Subscription"
+        verbose_name_plural = "Subscriptions"
 
     def __str__(self):
-        return self.name
+        return f"{self.subscription_package_id}: {self.customer_id}"
 
     def get_absolute_url(self):
         return reverse("subscription-detail", kwargs={"pk": self.pk})
