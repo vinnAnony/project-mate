05fe8a95e93a0675b04b9605dfcb64ba76c67dc9
Vincent Mbugua <vnjeru85@gmail.com>
2023-06-30 09:37:18 +0300

    add nested urls

--- backend/subscription/urls.py
+++ backend/subscription/urls.py
@@ -1,13 +1,26 @@
 from django.urls import path, include
 
-from rest_framework import routers
-from . import views
+from rest_framework_nested import routers
+from .views import *
 
 router = routers.SimpleRouter()
-router.register(r"subscriptions", views.SubscriptionViewset, basename="subscription")
-router.register(r"subscription-packages", views.SubscriptionPackageViewset, basename="subscription-package")
-router.register(r"invoices", views.InvoiceViewset, basename="invoice")
 
-urlpatterns = []
+router.register(r"subscription-packages", SubscriptionPackageViewset, basename="subscription-packages")
+router.register(r"subscriptions", SubscriptionViewset, basename="subscriptions")
+router.register(r"invoices", InvoiceViewset, basename="invoices")
 
-urlpatterns += router.urls
+# /subscription-packages/{id}/subscriptions/
+subscription_package_router = routers.NestedSimpleRouter(router, r'subscription-packages', lookup='subscription_package')
+subscription_package_router.register(r"subscriptions", SubscriptionViewset, basename="subscription-package-subscriptions")
+
+# /subscriptions/{id}/invoices/
+subscription_router = routers.NestedSimpleRouter(router, r'subscriptions', lookup='subscription')
+subscription_router.register(r"invoices", InvoiceViewset, basename="subscription-invoices")
+
+
+urlpatterns = [
+    path(r'', include(router.urls)),
+    path(r'', include(subscription_package_router.urls)),
+    path(r'', include(subscription_router.urls))
+]
+
