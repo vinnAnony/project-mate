88059d308b706d9d82611e6b98b73a27e7c2e077
Vincent Mbugua <vnjeru85@gmail.com>
2023-07-13 11:07:59 +0300

    update Dockerfile and entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,8 +4,6 @@ deploy(){
 
 	python manage.py migrate
 
-	echo "Admin F Name: ${DJANGO_SUPERUSER_FIRSTNAME}"
-
 	python manage.py createsuperuser --first_name=${DJANGO_SUPERUSER_FIRSTNAME} --last_name=${DJANGO_SUPERUSER_LASTNAME} --username ${DJANGO_SUPERUSER_USERNAME} --email ${DJANGO_SUPERUSER_EMAIL} --noinput
 
 	# celery -A bizcore worker
