551f5f5d514ab7dcab12ef907faa34a8b78b60eb
Vincent Mbugua <vnjeru85@gmail.com>
2023-07-13 10:59:41 +0300

    update Dockerfile and entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,12 +4,16 @@ deploy(){
 
 	python manage.py migrate
 
+	echo "Admin F Name: ${DJANGO_SUPERUSER_FIRSTNAME}"
+
 	python manage.py createsuperuser --first_name=${DJANGO_SUPERUSER_FIRSTNAME} --last_name=${DJANGO_SUPERUSER_LASTNAME} --username ${DJANGO_SUPERUSER_USERNAME} --email ${DJANGO_SUPERUSER_EMAIL} --noinput
 
 	# celery -A bizcore worker
 	# celery -A bizcore  beat
 
-    gunicorn bizcore.wsgi:application --bind 0.0.0.0:${APP_PORT}
+	python manage.py collectstatic
+
+    gunicorn bizcore.wsgi
 }
 
 deploy
