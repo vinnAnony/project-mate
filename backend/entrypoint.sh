74377b8080bb53a5106a6910d4daa094e5c6049f
Vincent Mbugua <vnjeru85@gmail.com>
2023-07-13 09:55:11 +0300

    update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,7 +6,10 @@ deploy(){
 
 	python manage.py createsuperuser --first_name=${DJANGO_SUPERUSER_FIRSTNAME} --last_name=${DJANGO_SUPERUSER_LASTNAME} --username ${DJANGO_SUPERUSER_USERNAME} --email ${DJANGO_SUPERUSER_EMAIL} --noinput
 
-    python manage.py runserver 0.0.0.0:${APP_PORT}
+	celery -A bizcore worker
+	celery -A bizcore  beat
+
+    gunicorn bizcore.wsgi:application --bind 0.0.0.0:${APP_PORT}
 }
 
 deploy
